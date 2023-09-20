ac0b9aee16c764612fed3401f76cacb37599cb58
Айса Эрендженова <a.erendzhenova@marine-rc.ru>
2023-09-20 03:29:59 +0300

    saved changes2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'foodgram.urls'
