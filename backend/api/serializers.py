bc692f91fd4de8f036fc8ee2e29073f2b3c883f0
Айса Эрендженова <a.erendzhenova@marine-rc.ru>
2023-09-24 15:23:07 +0300

    changed ingredienintrecipeserializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,7 +8,6 @@ from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-from rest_framework.fields import IntegerField
 from rest_framework.relations import PrimaryKeyRelatedField
 from rest_framework.serializers import ModelSerializer, ReadOnlyField
 from rest_framework.validators import UniqueTogetherValidator
