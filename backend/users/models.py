90b2b75f84aaa44453656fb6a466a420487df376
Айса Эрендженова <a.erendzhenova@marine-rc.ru>
2023-09-26 16:18:36 +0300

    saved changes in SubscriptionModel

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models import CheckConstraint, F, Q
+from django.db.models import CheckConstraint
 
 from foodgram.settings import EMAIL_LENGTH
 
