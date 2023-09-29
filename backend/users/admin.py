660462ffef100a039fc933917c0d1c7fd1dd3beb
Айса Эрендженова <a.erendzhenova@marine-rc.ru>
2023-09-29 22:43:54 +0300

    saved

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,10 +1,19 @@
+from django import forms
 from django.contrib import admin
 
 from .models import Subscription, User
 
 
+class UserChangeForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    form = UserChangeForm
     list_display = (
         'id', 'email',
         'username', 'first_name',
@@ -17,4 +26,5 @@ class UserAdmin(admin.ModelAdmin):
 
 @admin.register(Subscription)
 class SubscriptionAdmin(admin.ModelAdmin):
+    form = UserChangeForm
     list_display = ('id', 'user', 'author',)
