69a7034b8a70357be19162c298471344628d5de6
Айса Эрендженова <a.erendzhenova@marine-rc.ru>
2023-10-01 01:09:08 +0300

    saved after rebuild

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models import CheckConstraint
 from django.utils.translation import gettext_lazy as _
 
 from foodgram.settings import EMAIL_LENGTH
